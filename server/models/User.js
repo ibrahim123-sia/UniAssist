1c75ba327dcf481fa780f13fed20e0b0d58c7d46
Syed Ibrahim Ali <syedibrahimali1111@gmail.com>
2025-12-10 13:40:18 +0500

    .

--- server/models/User.js
+++ server/models/User.js
@@ -60,9 +60,9 @@ userSchema.pre("save", async function (next) {
   try {
     const salt = await bcrypt.genSalt(10);
     this.password = await bcrypt.hash(this.password, salt);
-    next();
+    next();  // ✅ Call next()
   } catch (error) {
-    next(error);
+    next(error);  // ✅ Pass error to next
   }
 });
 
